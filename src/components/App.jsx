9dfe8841ee4c622e5c7aee68fcc4f2a0735f692e
Finley <Finley.mwachia12@gmail.com>
2024-08-26 21:04:13 +0300

    project works

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,25 @@ import React, { useState } from "react";
 
 function App() {
   const [name, setName] = useState("");
-
+const [headingText,setHeadingText]= useState("");
   function handleChange(event) {
     setName(event.target.value);
   }
+
+  function handleSubmit(){
+    setHeadingText(name);
+  }
+
   return (
     <div className="container">
-      <h1>Hello {name} </h1>
+      <h1>Hello {headingText} </h1>
       <input
         onChange={handleChange}
         type="text"
         placeholder="What's your name?"
+        value={name}
       />
-      <button>Submit</button>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
